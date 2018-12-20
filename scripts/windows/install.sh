432e5d56746b9021a93329c87fd49438c9320dc8
Thomas Geissl <thomas.geissl@gmail.com>
2018-12-20 18:54:28 -0500

    added windows ci script

--- scripts/windows/install.sh
+++ scripts/windows/install.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-echo "TODO"
+#!/bin/sh
+
+curl -O curl https://openframeworks.cc/versions/v0.10.1/of_v0.10.1_msys2_release.zip
+unzip -a -qq of_v0.10.1_msys2_release.zip
+rm of_v0.10.1_msys2_release.zip
+./of_v0.10.1_msys2_release/scripts/msys2/install_dependencies.sh
+
+cd of_v0.10.1_msys2_release/apps
+mkdir ofPackageManager && cd ofPackageManager
+git clone https://github.com/thomasgeissl/ofPackageManager.git
+cd ofPackageManager/scripts
+./install_dependencies.sh
+cd ..
+make
