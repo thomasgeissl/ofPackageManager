042d1de26ed4759b09b592531e403630b0873cb6
Thomas Geissl <thomas.geissl@gmail.com>
2019-01-21 15:24:06 -0500

    updated test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-mkdir test && cd test
+mkdir test 
+cd test
 ofPackageManager info
 ofPackageManager install ofxMidi@1.1.1
 ofPackageManager install arturoc/ofxEasing
 rm -rf local_addons
 ofPackageManager install
+echo "" > addons.make
+ofPackageManager add -A local_addons
+rm -rf local_addons
+ofPackageManager install
+ofPackageManager install https://github.com/thomasgeissl/ofxPackageManageableExampleAddon.git
+
