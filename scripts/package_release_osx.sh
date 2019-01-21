0e79bc7392f50eaa8926918e1a4bc5b3c9ebd90c
Thomas Geissl <thomas.geissl@gmail.com>
2019-01-21 13:47:19 -0500

    fixed source path

--- scripts/package_release_osx.sh
+++ scripts/package_release_osx.sh
@@ -2,6 +2,6 @@
 mkdir ../../tmp
 cd ../../tmp
 rm -rf *
-cp -R ../../ofPackageManager ofPackageManager
+cp -R ../ofPackageManager ofPackageManager
 zip -r ofPackageManager_osx_version.zip ofPackageManager
 openssl dgst -sha256 ofPackageManager_osx_version.zip
