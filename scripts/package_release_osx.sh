dec05c78ebf1a52e329bfdcd32b3bfbc059b26ec
Thomas Geissl <thomas.geissl@gmail.com>
2019-01-21 04:30:28 -0500

    added checksum output

--- scripts/package_release_osx.sh
+++ scripts/package_release_osx.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 mkdir ../../tmp && cd ../../tmp
 cp -R ../../ofPackageManager ofPackageManager
-zip -r ofPackageManager_version_osx.zip ofPackageManager
+zip -r ofPackageManager_osx_version.zip ofPackageManager
+openssl dgst -sha256 ofPackageManager_osx_version.zip
