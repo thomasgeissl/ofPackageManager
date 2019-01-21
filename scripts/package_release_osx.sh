ff052f5a56866dad12617bda48508a47729e9427
Thomas Geissl <thomas.geissl@gmail.com>
2019-01-21 13:41:32 -0500

    fixed packaing script

--- scripts/package_release_osx.sh
+++ scripts/package_release_osx.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-mkdir ../../tmp && cd ../../tmp
+mkdir ../../tmp
+cd ../../tmp
+rm -rf *
 cp -R ../../ofPackageManager ofPackageManager
 zip -r ofPackageManager_osx_version.zip ofPackageManager
 openssl dgst -sha256 ofPackageManager_osx_version.zip
