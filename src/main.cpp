de4381da5ee546119661f717f19d0ec6f690ced9
Thomas Geissl <thomas.geissl@gmail.com>
2019-09-13 15:28:36 +0200

    cross platform cwd path

--- src/main.cpp
+++ src/main.cpp
@@ -2,99 +2,142 @@
 #include "ofxUnitTests.h"
 #include "ofApp.h"
 
-void printInputError(int argc, char** argv){
+void printInputError(int argc, char **argv)
+{
 	std::string input;
-	for(auto i = 1; i < argc -1; i++){
+	for (auto i = 1; i < argc - 1; i++)
+	{
 		input += argv[i];
 		input += "";
 	}
 	ofLogError("ofPackageManager") << "unkown command:" << input;
 }
 
-int main(int argc, char** argv){
-	#if defined(TARGET_OSX) || defined(TARGET_LINUX)
-		std::shared_ptr <ofColorsLoggerChannel> logger {
-			new ofColorsLoggerChannel
-		};
-		ofSetLoggerChannel(logger);
-	#endif
-//    ofSetLogLevel(OF_LOG_VERBOSE);
-	char * cwdPath;
-	char buff[PATH_MAX + 1];
-	cwdPath = getcwd(buff, PATH_MAX + 1);
-	ofPackageManager app(cwdPath);
+int main(int argc, char **argv)
+{
+#if defined(TARGET_OSX) || defined(TARGET_LINUX)
+	std::shared_ptr<ofColorsLoggerChannel> logger{
+		new ofColorsLoggerChannel};
+	ofSetLoggerChannel(logger);
+#endif
+	//    ofSetLogLevel(OF_LOG_VERBOSE);
+	filesystem::path cwdPath = filesystem::current_path();
+
+	ofPackageManager app(cwdPath.string());
 
 	std::string task;
-	if(argc > 1){
+	if (argc > 1)
+	{
 		task = argv[1];
 	}
 
-	if(task == "add"){
-		if(argc == 3){
+	if (task == "add")
+	{
+		if (argc == 3)
+		{
 			app.addPackageToAddonsMakeFile(argv[2]);
-		}else if(argc == 4){
+		}
+		else if (argc == 4)
+		{
 			string option = argv[2];
-			if(option == "-A" || option == "--all"){
-			app.addPackagesToAddonsMakeFile(argv[3]);
+			if (option == "-A" || option == "--all")
+			{
+				app.addPackagesToAddonsMakeFile(argv[3]);
 			}
-		}else{
+		}
+		else
+		{
 			ofLogError("ofPackageManager") << "invalid syntax";
 		}
-	}else if(task == "config"){
-		if(argc == 2){
+	}
+	else if (task == "config")
+	{
+		if (argc == 2)
+		{
 			app.configurePackageManager();
 		}
-		if(argc == 3){
+		if (argc == 3)
+		{
 			std::string option = argv[2];
-			if(option == "-g" || option == "--global"){
+			if (option == "-g" || option == "--global")
+			{
 				app.configurePackageManager(true);
 			}
 		}
-	}else if(task == "doctor"){
+	}
+	else if (task == "doctor")
+	{
 		app.doctor();
-	}else if(task == "generate"){
+	}
+	else if (task == "generate")
+	{
 		std::string subtask = "project";
 
-		if(argc == 3){
+		if (argc == 3)
+		{
 			subtask = argv[2];
-		}else if(argc > 3){
+		}
+		else if (argc > 3)
+		{
 			printInputError(argc, argv);
 			app.printManual();
 			return -1;
 		}
 
-		if(subtask == "project"){
+		if (subtask == "project")
+		{
 			app.generateProject();
-		}else if(subtask == "readme"){
+		}
+		else if (subtask == "readme")
+		{
 			app.generateReadme();
-		}else if(subtask == "database"){
+		}
+		else if (subtask == "database")
+		{
 			app.generateDatabaseEntryFile();
-		}else{
+		}
+		else
+		{
 			printInputError(argc, argv);
 			app.printManual();
 			return -1;
 		}
-	}else if(task == "help"){
+	}
+	else if (task == "help")
+	{
 		app.printManual();
-	}else if(task == "info"){
+	}
+	else if (task == "info")
+	{
 		app.printInfo();
-	}else if(task == "install"){
-		if(argc == 2){
+	}
+	else if (task == "install")
+	{
+		if (argc == 2)
+		{
 			app.installPackagesFromAddonsMakeFile();
-		}else if(argc >= 3){
+		}
+		else if (argc >= 3)
+		{
 			std::string package;
 			std::string destinationPath = "local_addons";
 			bool global = false;
-			if(argc == 3){
+			if (argc == 3)
+			{
 				package = argv[2];
-			}else if(argc == 4){
+			}
+			else if (argc == 4)
+			{
 				std::string option = argv[2];
-				if(option == "-g" || option == "--global"){
+				if (option == "-g" || option == "--global")
+				{
 					destinationPath = ofFilePath::join(app.getOfPath(), "addons");
 					global = true;
 				}
 				package = argv[3];
-			}else{
+			}
+			else
+			{
 				printInputError(argc, argv);
 				app.printManual();
 				return -1;
@@ -102,51 +145,77 @@ int main(int argc, char** argv){
 
 			auto installedPackage = app.installPackage(package, destinationPath);
 			// TODO: move to app
-			if (!global && !installedPackage._path.empty()) {
+			if (!global && !installedPackage._path.empty())
+			{
 				app.addPackageToAddonsMakeFile(installedPackage);
-			}	
+			}
 		}
-	}else if(task == "print"){
+	}
+	else if (task == "print")
+	{
 		std::string subtask = "packages";
-		if(argc == 3){
+		if (argc == 3)
+		{
 			subtask = argv[2];
 		}
-		if(subtask == "packages"){
+		if (subtask == "packages")
+		{
 			app.printAvailablePackages();
-		}else if(subtask == "manual" || subtask == "man" || subtask == "help"){
+		}
+		else if (subtask == "manual" || subtask == "man" || subtask == "help")
+		{
 			app.printManual();
-		}else if(subtask == "version"){
+		}
+		else if (subtask == "version")
+		{
 			app.printVersion();
-		}else{
+		}
+		else
+		{
 			printInputError(argc, argv);
 			app.printManual();
 			return -1;
 		}
-
-	}else if(task == "search"){
-		if(argc == 3){
+	}
+	else if (task == "search")
+	{
+		if (argc == 3)
+		{
 			app.searchPackageInDatabaseById(argv[2]);
-		}else if(argc == 4){
+		}
+		else if (argc == 4)
+		{
 			std::string subtask = argv[2];
-			if(subtask == "github"){
+			if (subtask == "github")
+			{
 				app.searchPackageOnGithubByName(argv[3]);
 			}
-		}else if(argc == 5){
+		}
+		else if (argc == 5)
+		{
 			std::string subtask = argv[2];
-			if(subtask == "github"){
+			if (subtask == "github")
+			{
 				string option = argv[3];
-				if(option == "-u" || option == "--user"){
+				if (option == "-u" || option == "--user")
+				{
 					app.searchPackageOnGithubByUser(argv[4]);
 				}
 			}
-		}else{
+		}
+		else
+		{
 			printInputError(argc, argv);
 			app.printManual();
 			return -1;
 		}
-	}else if(task == "update"){
+	}
+	else if (task == "update")
+	{
 		app.updatePackagesDatabase();
-	}else{
+	}
+	else
+	{
 		ofLogError("ofPackageManager") << "unkown task" << task;
 		app.printInfo();
 		app.printManual();
