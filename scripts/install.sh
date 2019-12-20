bef0849a92e2fb44575b1250ca7f7c7238d7e9d2
Thomas Geissl <thomas.geissl@gmail.com>
2019-12-20 19:52:29 +0100

    updated to oF 0.11.0

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 # TODO: automatically get os
+VERSION=0.11.0
 echo "What OS are you using? (0): OSX, (1): LINUX, (any other): WINDOWS"
 read -r OS
 if [ $OS = 0 ]
 then 
-    curl -O https://openframeworks.cc/versions/v0.10.1/of_v0.10.1_osx_release.zip
-    unzip -a -qq of_v0.10.1_osx_release.zip
-    rm of_v0.10.1_osx_release.zip
-    cd of_v0.10.1_osx_release/apps
+    curl -O https://openframeworks.cc/versions/v${VERSION}/of_v${VERSION}_osx_release.zip
+    unzip -a -qq of_v${VERSION}_osx_release.zip
+    rm of_v${VERSION}_osx_release.zip
+    cd of_v${VERSION}_osx_release/apps
     mkdir packageManager
     cd packageManager
 elif [ $OS = 1 ]
 then
-        curl -O https://openframeworks.cc/versions/v0.10.1/of_v0.10.1_linux64gcc6_release.tar.gz
-        tar -zxf of_v0.10.1_linux64gcc6_release.tar.gz
-        rm of_v0.10.1_linux64gcc6_release.tar.gz
-        of_v0.10.1_linux64gcc6_release/scripts/linux/ubuntu/install_dependencies.sh
-        cd of_v0.10.1_linux64gcc6_release/apps
+        curl -O https://openframeworks.cc/versions/v${VERSION}/of_v${VERSION}_linux64gcc6_release.tar.gz
+        tar -zxf of_v${VERSION}_linux64gcc6_release.tar.gz
+        rm of_v${VERSION}_linux64gcc6_release.tar.gz
+        of_v${VERSION}_linux64gcc6_release/scripts/linux/ubuntu/install_dependencies.sh
+        cd of_v${VERSION}_linux64gcc6_release/apps
         mkdir packageManager
         cd packageManager
 else echo "windows support is not yet implemented" && exit
