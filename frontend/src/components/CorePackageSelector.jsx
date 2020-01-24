172df613480286a8bb94ea5ee234731751ff31f8
Thomas Geissl <thomas.geissl@gmail.com>
2020-01-24 16:13:39 +0100

    added oF svg icon for all core addons

--- frontend/src/components/CorePackageSelector.jsx
+++ frontend/src/components/CorePackageSelector.jsx
@@ -3,7 +3,6 @@ import { useSelector, useDispatch } from "react-redux";
 import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import SvgIcon from "@material-ui/core/SvgIcon";
-import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
 import styled from "styled-components";
 import { addCoreAddon, removeCoreAddon } from "../state/reducers/corePackages";
 
@@ -48,11 +47,44 @@ export default () => {
                 }
                 label={name}
               />
-              {/* TODO: oF icon */}
-              {/* <SvgIcon>
-                <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-              </SvgIcon> */}
-              <CheckCircleOutlineIcon fontSize="small"></CheckCircleOutlineIcon>
+              <SvgIcon viewBox="0 0 256 256">
+                <g
+                  id="logo"
+                  stroke="none"
+                  strokeWidth="1"
+                  fill="none"
+                  fillRule="evenodd"
+                >
+                  <g
+                    id="Group"
+                    transform="translate(6.000000, 63.000000)"
+                    fill="#000000"
+                  >
+                    <rect
+                      id="Rectangle"
+                      stroke="#979797"
+                      x="189"
+                      y="58"
+                      width="34"
+                      height="34"
+                    ></rect>
+                    <rect
+                      id="Rectangle"
+                      stroke="#979797"
+                      x="132"
+                      y="1"
+                      width="54"
+                      height="128"
+                    ></rect>
+                    <polygon
+                      id="Triangle"
+                      stroke="#979797"
+                      points="189 1 242 1 189 54"
+                    ></polygon>
+                    <circle id="Oval" cx="65" cy="65" r="65"></circle>
+                  </g>
+                </g>
+              </SvgIcon>
             </li>
           );
         })}
