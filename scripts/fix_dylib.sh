d9c4cbe79b085c39b0a61573fa52d1cf01ff7747
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-13 18:28:40 +0200

    trying to build on osx

--- scripts/fix_dylib.sh
+++ scripts/fix_dylib.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+cwd=$(pwd)
 cd "$(dirname "$0")"
 
 cp ../local_addons/ofxGit2/libs/libgit2/lib/osx/libgit2.27.dylib ../bin/ofPackageManager.app/Contents/MacOS/
 install_name_tool -change @rpath/libgit2.27.dylib @executable_path/libgit2.27.dylib ../bin/ofPackageManager.app/Contents/MacOS/ofPackageManager
+
+cd $pwd
