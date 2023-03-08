b64a7dc83de8beecf584e57809e299c8815ab11e
Thomas Geissl <thomas.geissl@gmail.com>
2023-03-08 18:21:26 +0100

    updated ofxgit2

--- addons.make
+++ addons.make
@@ -1,7 +1,9 @@
 ofxUnitTests
 ofxPoco
 local_addons/ofxCommandLineUtils #https://github.com/thomasgeissl/ofxCommandLineUtils.git@734be727c
-local_addons/ofxGit2 #https://github.com/thomasgeissl/ofxGit2.git@2c197469c
+local_addons/ofxGit2 #git@github.com:thomasgeissl/ofxGit2.git@9e0e5b83d
 local_addons/ofxImGui #git@github.com:Daandelange/ofxImGui.git@337351aee
 local_addons/ofxStateMachine #https://github.com/thomasgeissl/ofxStateMachine.git@1574209df
+
+df
 
--- scripts/fix_dylib.sh
+++ scripts/fix_dylib.sh
@@ -2,8 +2,9 @@
 cwd=$(pwd)
 cd "$(dirname "$0")"
 
-cp ../local_addons/ofxGit2/libs/libgit2/lib/osx/libgit2.27.dylib ../bin/ofPackageManager.app/Contents/MacOS/
-install_name_tool -change @rpath/libgit2.27.dylib @executable_path/libgit2.27.dylib ../bin/ofPackageManager.app/Contents/MacOS/ofPackageManager
+cp ../local_addons/ofxGit2/libs/libgit2/lib/osx/libgit2.1.5.dylib ../bin/ofPackageManager.app/Contents/MacOS/
+cd ../bin/ofPackageManager.app/Contents/MacOS/
+install_name_tool -change @rpath/libgit2.1.5.dylib @executable_path/libgit2.1.5.dylib ./ofPackageManager
 
 cd $pwd
 
